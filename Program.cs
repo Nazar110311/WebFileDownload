98aee40e2a1c559bed8400b12c132f29d6372fd0
nazar <nazar@NAZAR>
2022-09-26 22:18:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebDownload webDownload = new WebDownload();
+            webDownload.DownloadWebFile(@"https://download.virtualbox.org/virtualbox/6.1.38/VirtualBox-6.1.38-153438-Win.exe", @"C:\Users\nazar\VirtualBox.exe");
+            for(int i = 0; i > -1; i++)
+            {
+                Console.WriteLine($"{webDownload.Percentages}% {webDownload.SizeFile}МБ {webDownload.FileDownloadBytes}");
+                if(webDownload.Percentages == "100")
+                {
+                    break;
+                }
+                System.Threading.Thread.Sleep(900);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class WebDownload
+    {
+        public string Percentages;
+        public string FileDownloadBytes;
+        public string SpeedBytes;
+        public string SizeFile;
+
+        public void DownloadWebFile(string url, string savepath)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.OpenRead(url);
+                string size = (Convert.ToDouble(wc.ResponseHeaders["Content-Length"]) / 1048576).ToString("#.#");
+
+                wc.DownloadProgressChanged += (s, e) =>
+                {
+                    SizeFile = size;
+                    FileDownloadBytes = ((do) e.BytesReceived / 1048576).ToString("#.#");
+
+                    Percentages = e.ProgressPercentage.ToString();
+                };
+                wc.DownloadFileAsync(new Uri(url), savepath);
+            }
+        }
+    }
+} 
